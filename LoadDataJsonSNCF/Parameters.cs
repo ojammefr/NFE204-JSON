dea70a5c425937d6d5ba1fd4698846d9397d6869
ojammefr <ojammefr@yahoo.fr>
2020-07-11 17:50:45 +0200

    C'est ok

---
+++ LoadDataJsonSNCF/Parameters.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LoadDataJsonSNCF
+{
+    public class Parameters
+    {
+        public string dataset { get; set; }
+        public string timezone { get; set; }
+        public int rows { get; set; }
+        public string format { get; set; }
+        public List<string> facet { get; set; }
+    }
+
+    public class Fields
+    {
+        public string gc_obo_nature_c { get; set; }
+        public string gc_obo_gare_origine_r_name { get; set; }
+        public DateTime date { get; set; }
+        public string gc_obo_nom_recordtype_sc_c { get; set; }
+        public string gc_obo_type_c { get; set; }
+        public string gc_obo_gare_origine_r_code_uic_c { get; set; }
+    }
+
+    public class Record
+    {
+        public string datasetid { get; set; }
+        public string recordid { get; set; }
+        public Fields fields { get; set; }
+        public DateTime record_timestamp { get; set; }
+    }
+
+    public class RecDB
+    {
+        public string datasetid { get; set; }
+        public string recordid { get; set; }
+        public string gc_obo_nature_c { get; set; }
+        public string gc_obo_gare_origine_r_name { get; set; }
+        public DateTime date { get; set; }
+        public string gc_obo_nom_recordtype_sc_c { get; set; }
+        public string gc_obo_type_c { get; set; }
+        public string gc_obo_gare_origine_r_code_uic_c { get; set; }
+        public DateTime record_timestamp { get; set; }
+    }
+
+    public class Facet
+    {
+        public int count { get; set; }
+        public string path { get; set; }
+        public string state { get; set; }
+        public string name { get; set; }
+    }
+
+    public class FacetGroup
+    {
+        public List<Facet> facets { get; set; }
+        public string name { get; set; }
+    }
+
+    public class RootObject
+    {
+        public string datasetid { get; set; }
+        public string recordid { get; set; }
+        public Fields fields { get; set; }
+        public DateTime record_timestamp { get; set; }
+    }
+}
---
+++ LoadDataJsonSNCF/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.IO;
+using System.Collections.Generic;
+using Raven.Client.Documents;
+using Raven.Client.Documents.BulkInsert;
+
+namespace LoadDataJsonSNCF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var store = new DocumentStore
+            {
+                Urls = new[] { "http://localhost:8080" },
+                Database = "Objets_Trouves"
+            };
+            store.Initialize();
+
+            using (BulkInsertOperation bulkInsert = store.BulkInsert())
+            using (StreamReader sr = new StreamReader("/Volumes/MicroSD/Olivier/1.Cnam/01.NFE204/donnees_projet/objets-trouves-restitution.json"))
+            using (JsonReader reader = new JsonTextReader(sr))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+
+                List<Record> rec = serializer.Deserialize<List<Record>>(reader);
+                foreach (var repo in rec)
+                {
+                    var recdb = new RecDB
+                    {
+
+                        datasetid = repo.datasetid,
+                        recordid = repo.recordid,
+                        record_timestamp = repo.record_timestamp,
+                        gc_obo_gare_origine_r_code_uic_c = repo.fields.gc_obo_gare_origine_r_code_uic_c,
+                        gc_obo_nature_c = repo.fields.gc_obo_nature_c,
+                        gc_obo_gare_origine_r_name = repo.fields.gc_obo_gare_origine_r_name,
+                        date = repo.fields.date,
+                        gc_obo_nom_recordtype_sc_c = repo.fields.gc_obo_nom_recordtype_sc_c,
+                        gc_obo_type_c = repo.fields.gc_obo_type_c
+
+                    };
+                    bulkInsert.Store(recdb);
+                }
+                Console.WriteLine("FIN");
+            }
+        }
+    }
+}
